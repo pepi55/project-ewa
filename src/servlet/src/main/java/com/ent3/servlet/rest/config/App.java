33a1b6818ec9714bcd423c69bac11cd9d5e29793
Peter Dimitrov <peter.dimitrov@hva.nl>
2018-11-01 18:31:21 +0100

    Added default config and test resource

---
+++ src/servlet/src/main/java/com/ent3/servlet/rest/config/App.java
@@ -0,0 +1,11 @@
+package com.ent3.servlet.rest.config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * App config
+ */
+@ApplicationPath("services/rest")
+public class App extends ResourceConfig {
+}
---
+++ src/servlet/src/main/java/com/ent3/servlet/rest/resource/TestResource.java
@@ -0,0 +1,19 @@
+package com.ent3.servlet.rest.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * TestResource
+ */
+@Path("test")
+public class TestResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String resourceTest() {
+        return "Testing123";
+    }
+}
