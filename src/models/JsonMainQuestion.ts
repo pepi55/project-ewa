d7457d77128f0f816540cf53eacdb40d52e39180
hicham <hicham_13@live.nl>
2018-11-20 10:16:04 +0100

    Revert "Merge remote-tracking branch 'origin/extra-signup-options'"
    This reverts commit 3fe7da4148824490a7ada45e870f6f162f351f65.

--- src/models/JsonMainQuestion.ts
+++
@@ -1,7 +0,0 @@
-import { JsonSubQuestion } from "./JsonSubQuestion";
-
-export class JsonMainQuestion {
-    public mainQuestion: string;
-    public answers : Array<string>;
-    public subQuestions: Array<JsonSubQuestion>;
-}
--- src/servlet/src/main/java/com/ent3/servlet/rest/resource/UserResource.java
+++ src/servlet/src/main/java/com/ent3/servlet/rest/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.ent3.servlet.rest.resource;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -9,6 +10,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -19,10 +21,12 @@ import com.ent3.servlet.service.implementation.RepoImplementation;
 
 /**
  * UserResource
+ *
+ * @author Peter Dimitrov
  */
 @Path("users")
 public class UserResource {
-    private RepositoryService service;
+    private UserRepository service;
 
     public UserResource() {
         // XXX: Repo implementation class here.
@@ -31,8 +35,38 @@ public class UserResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<User> getAllUsers() {
-        return service.getAllUsers();
+    public Response getAllUsers(@DefaultValue("") @QueryParam("firstname") String firstname, @DefaultValue("") @QueryParam("lastname") String lastname) {
+        List<User> result = service.getAllUsers();
+
+        if (result == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity(new ClientError("No users found")).build();
+        }
+
+        if (!firstname.isEmpty()) {
+            List<User> tmp = new ArrayList<>();
+
+            for (User user : result) {
+                if (firstname.equals(user.getFirstName())) {
+                    tmp.add(user);
+                }
+            }
+
+            result = tmp;
+        }
+
+        if (!lastname.isEmpty()) {
+            List<User> tmp = new ArrayList<>();
+
+            for (User user : result) {
+                if (lastname.equals(user.getLastName())) {
+                    tmp.add(user);
+                }
+            }
+
+            result = tmp;
+        }
+
+        return Response.status(Response.Status.OK).entity(result).build();
     }
 
     @GET
