c8476a30fa7731f795218ee9804b4980e6d0252e
Peter Dimitrov <peter.dimitrov@hva.nl>
2018-11-23 02:27:45 +0100

    Code cleanup

--- src/servlet/src/main/java/com/ent3/servlet/rest/resource/QuestionResource.java
+++ src/servlet/src/main/java/com/ent3/servlet/rest/resource/QuestionResource.java
@@ -1,23 +1,12 @@
 package com.ent3.servlet.rest.resource;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.ent3.servlet.model.Question;
-import com.ent3.servlet.rest.model.ClientError;
 import com.ent3.servlet.service.QuestionRepository;
-import com.ent3.servlet.service.UserRepository;
 import com.ent3.servlet.service.implementation.RepoImplementation;
 
 /**
@@ -28,7 +17,7 @@ import com.ent3.servlet.service.implementation.RepoImplementation;
 @Path("questions")
 public class QuestionResource {
     private QuestionRepository service;
-    
+
     public QuestionResource(){
         service = RepoImplementation.getInstance();
     }
