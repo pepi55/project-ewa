dd216b980e5bcde3c274d6171daf08341a9b9bef
Toon <toon.de.hoop@hva.nl>
2018-11-26 01:26:57 +0100

    Merge test with testpage

--- src/TS/app.ts
+++ src/TS/app.ts
@@ -2,6 +2,7 @@ import { MainController } from "./controllers/maincontroller";
 import { LoginController } from "./controllers/logincontroller";
 import { adminCourseController } from "./controllers/adminCourseController";
 import { userCourseController } from "./controllers/userCourseController";
+import { TestController} from "./controllers/testcontroller";
 
 
 export class App {
@@ -15,6 +16,8 @@ export class App {
             let controller : adminCourseController = new adminCourseController();
         } else if (type == "userCourses") {
             let controller : userCourseController = new userCourseController();
+        } else if (type == "test"){
+            let controller: TestController = new TestController();
         }
     }
 }
---
+++ src/TS/components/area.ts
@@ -0,0 +1,15 @@
+export class Area{
+    public area: string;
+    public areas: Array<Area>;
+
+   
+
+    public constructor(area: string, areas: Array<Area>){
+        this.area = area;
+        this.areas = areas;
+        
+    }
+
+    
+    
+}
