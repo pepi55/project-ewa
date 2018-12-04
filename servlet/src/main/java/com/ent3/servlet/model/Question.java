92de1dfc87a514b96665a148823158d9ec2f2356
Toon <toon.de.hoop@hva.nl>
2018-12-04 14:40:11 +0100

    test

--- servlet/src/main/java/com/ent3/servlet/model/Question.java
+++ servlet/src/main/java/com/ent3/servlet/model/Question.java
@@ -1,29 +1,59 @@
 package com.ent3.servlet.model;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 /**
  * Representation of a Question
  *
  * @author Toon de Hoop
  */
 
- public class Question{
-     private String question; 
-     private Competency competency;
-     private int score;
+@Entity
+@Table(name = "questions")
+public class Question implements Serializable {
+    @Transient
+    private static final long serialVersionUID = 1L;
 
-     public Question(String question, Competency competency){
-         this.question = question;
-         this.competency = competency;
-         this.score = 0;
-     }
+    @Id
+    @Column(name = "question_id")
+    @GeneratedValue
+    private int id;
 
-     public String getQuestion() {
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "competency_id")
+    private List<Competency> competences;
+
+    private String question;
+    private Competency competency;
+    private int score;
+
+    public Question(String question, Competency competency) {
+        this.question = question;
+        this.competency = competency;
+        this.score = 0;
+    }
+
+    public String getQuestion() {
         return this.question;
     }
 
     public void setQuestion(String question) {
         this.question = question;
     }
+
     public Competency getCompetency() {
         return this.competency;
     }
@@ -40,9 +70,3 @@ package com.ent3.servlet.model;
         this.score = score;
     }
 }
- 
-     
-
-         
-     
- 
