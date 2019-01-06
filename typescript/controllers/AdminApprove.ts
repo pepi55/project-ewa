db4527701f07d3d4a753a6c58b860b44be467159
Luc Maerten <lucmaerten7@gmail.com>
2019-01-07 00:38:05 +0100

    fixed major bug in login and added approval screen for admins

---
+++ typescript/controllers/AdminApprove.ts
@@ -0,0 +1,66 @@
+import { User } from "../components/User";
+import { UserCard } from "../components/UserCard";
+import { Button } from "../components/button/Button";
+import { ApiService } from "../coursesAPIs/ApiService";
+import { API } from "../coursesAPIs/EnumRepo";
+import { Controller } from "./Controller";
+declare var componentHandler: any;
+
+export class AdminApprove extends Controller {
+
+    protected setup(): void {
+        console.log("hallo?")
+        let url = "http://127.0.0.1:8080/servlet/services/rest/users?approve=not_approved";
+        let promise = fetch(url);
+        promise.then(function (result) {
+            return result.json();
+        }).then(function (json: any) {
+            let i = 0;
+            for (let user of json) {
+                let newuser = new User(user["email"], user["firstName"], user["lastName"], user["username"], user["password"], user["role"]);
+                $("#admin_approve").append(new UserCard(newuser, i).getView());
+                componentHandler.upgradeDom();
+                let acceptButton = new Button("Accept");
+                acceptButton.setOnClick((e: any) => {
+                    console.log("clicked")
+                    let url = "http://localhost:8080/servlet/services/rest/users/" + user["username"] + "?approve=true";
+                    let promise = fetch(url);
+                    promise.then(function (result) {
+                        return result.json();
+                    }).then(function (json: any) {
+                        console.log(json)
+                    }.bind(this))
+                    $("#newUser" + i).remove();
+                    console.log("removed #newUser" + i)
+                    componentHandler.upgradeDom();
+
+                    // Ghetto fix
+                    location.reload();
+                })
+                let denyButton = new Button("Deny");
+                denyButton.setOnClick((e: any) => {
+                    let DB = new ApiService(API.DB);
+                    DB.setPath("users/" + user["username"]);
+                    DB.delete(<T>(object: any) => {
+                        console.log(object);
+                        if (object.statusCode == 200) {
+                            console.log("denied")
+                            $("#newUser" + i).remove();
+                            console.log("removed #newUser" + i)
+                            componentHandler.upgradeDom();
+
+                            // Ghetto fix
+                            this.location.reload();
+                        }
+                    })
+                })
+
+                $("#button_area" + i).append(acceptButton.getView())
+                    .append('<div style="width:20px; display: inline-block"></div>')
+                    .append(denyButton.getView());
+                i++;
+            }
+        });
+        componentHandler.upgradeDom();
+    }
+}
--- typescript/controllers/Menu.ts
+++ typescript/controllers/Menu.ts
@@ -42,6 +42,10 @@ export class Menu {
         let teacherClassItem: MenuItem = new MenuItem("My Class");
         teacherClassItem.setOnClick((e: any) => { window.location.href = "/servlet/views/teacherClass.html"; })
 
+        //admins
+        let adminApproveItem: MenuItem = new MenuItem("New Approvals");
+        adminApproveItem.setOnClick((e: any) => { window.location.href = "/servlet/views/adminApprove.html"; })
+
         //setting buttons for the menu
         switch (role) {
             case UserRole.ADMIN:
@@ -49,11 +53,13 @@ export class Menu {
                 this.onBar.push(newCoursesItem.getView());
                 this.onBar.push(savedCoursesItem.getView());
                 this.onBar.push(AdminTestItem.getView());
+                this.onBar.push(adminApproveItem.getView());
 
                 //onpage
                 this.onPage.push(newCoursesItem.getView());
                 this.onPage.push(savedCoursesItem.getView());
                 this.onPage.push(AdminTestItem.getView());
+                this.onPage.push(adminApproveItem.getView());
                 break;
 
             case UserRole.USER:
